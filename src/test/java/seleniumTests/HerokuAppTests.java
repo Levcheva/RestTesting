ea449d716a169ed31ec0455a8995208b59da0cb6
Kam Levchev <kameliya.levcheva@mentormate.com>
2022-06-19 01:37:56 +0300

    Lecture 19 Homework

--- src/test/java/seleniumTests/HerokuAppTests.java
+++ src/test/java/seleniumTests/HerokuAppTests.java
@@ -3,12 +3,16 @@ package seleniumTests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -18,24 +22,30 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 
 public class HerokuAppTests {
 
     WebDriver driver;
     WebDriverWait wait;
     Actions actions;
+    JavascriptExecutor js;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        // implicit wait - wait certain amount of time before throwing no such element exception
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
         actions = new Actions(driver);
+        js = (JavascriptExecutor) driver;
+        //explicit wait - add custom conditions, which should be met.
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 
@@ -43,18 +53,28 @@ public class HerokuAppTests {
     public void addRemoveElements() {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
 
-        List<WebElement> elementsContainer = driver.findElements(By.xpath("//div[@id='elements']/descendant::*"));
+        // List is now 0.
+        List<WebElement> elementsContainer = driver.findElements(By.cssSelector(".added-manually"));
         Assert.assertTrue(elementsContainer.isEmpty());
 
         WebElement addElementButton = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             addElementButton.click();
         }
-        elementsContainer = driver.findElements(By.xpath("//div[@id='elements']/descendant::*"));
 
-        Assert.assertEquals(elementsContainer.size(), 2);
-//        WebElement deleteButton = driver.findElement(By.cssSelector(".added-manually"));
+        // Same list is now not 0 but 3.
+        elementsContainer = driver.findElements(By.cssSelector(".added-manually"));
+
+        // Click the delete elements button to create delete buttons
+        for (WebElement deleteButton : elementsContainer
+        ) {
+            deleteButton.click();
+        }
+
+        List<WebElement> deleteButtonsAfter = driver.findElements(By.cssSelector(".added-manually"));
+
+        Assert.assertEquals(deleteButtonsAfter.size(), 0);
     }
 
     @Test
@@ -111,12 +131,131 @@ public class HerokuAppTests {
 
         if (checkbox1State) {
             checkbox1.click();
-        }else {
+        } else {
             checkbox1.click();
 
         }
         Assert.assertEquals(checkbox1State, checkbox1.isSelected());
     }
+
+    @Test
+    public void floatingMenu() {
+        driver.get("http://the-internet.herokuapp.com/floating_menu");
+        // assert floating element is there when opening the page
+        WebElement homeButton = driver.findElement(By.xpath("//div[@id='menu']//a[text()='Home']"));
+        Assert.assertTrue(homeButton.isDisplayed());
+
+        // scroll the page down
+        js.executeScript("window.scrollBy(0,2000)");
+
+        // assert floating element is still displayed after the scroll
+        Assert.assertTrue(homeButton.isDisplayed());
+
+        js.executeScript("window.scrollBy(0,-1000)");
+
+        js.executeScript("arguments[0].click()", homeButton);
+
+        wait.until(ExpectedConditions.visibilityOf(homeButton));
+
+        // assert floating element is still displayed after the scroll
+        Assert.assertTrue(homeButton.isDisplayed());
+    }
+
+    @Test
+    public void dynamicControls() {
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+
+        // assert the checkbox is present after loading the page
+        WebElement checkBox = driver.findElement(By.cssSelector("#checkbox"));
+        Assert.assertTrue((checkBox.isDisplayed()));
+
+        // click the remove button
+        WebElement removeButton = driver.findElement(By.xpath("//button[text()='Remove']"));
+        removeButton.click();
+
+        // wait until the animation for removing the checkbox is gone
+        WebElement loadingAnimation = driver.findElement(By.xpath("//div[@id='loading']"));
+        wait.until(ExpectedConditions.invisibilityOf(loadingAnimation));
+
+        WebElement message = driver.findElement(By.xpath("//p[@id='message']"));
+        wait.until(ExpectedConditions.invisibilityOf(checkBox));
+        Assert.assertEquals(message.getText(), "It's gone!");
+
+        // fluent wait example
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement loadingBar = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.xpath("//div[@id='loading']"));
+            }
+        });
+    }
+
+    @Test
+    public void dynamicLoading() {
+        driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
+        By startButton = By.xpath("//div[@id='start']/button");
+        By helloWorldText = By.xpath("//div[@id='finish']");
+
+        WebElement startButtonWebElement = driver.findElement(startButton);
+        startButtonWebElement.click();
+
+        WebElement hellowWorldText = driver.findElement(helloWorldText);
+    }
+
+    @Test
+    public void switchWindows() {
+        driver.get("http://the-internet.herokuapp.com/windows");
+        String firstWindowHandle = driver.getWindowHandle();
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        for (String windHandle:driver.getWindowHandles()
+        ) {
+            driver.switchTo().window(windHandle);
+        }
+    }
+
+    @Test
+    public void iFrames() {
+        driver.get("http://the-internet.herokuapp.com/iframe");
+
+        // step into the frame in which the web element is located
+        driver.switchTo().frame("mce_0_ifr");
+        // now that we are in the frame, we are able to save the locator to a web element
+        WebElement textElement = driver.findElement(By.xpath("//*[@id='tinymce']//p"));
+        textElement.clear();
+        textElement.sendKeys("some text");
+
+        // switch back to the main document or first frame
+        driver.switchTo().defaultContent();
+        WebElement headerText = driver.findElement(By.xpath("//div[@class='example']//h3"));
+        Assert.assertTrue(headerText.isDisplayed());
+    }
+
+    @Test
+    public void nestedFrames() {
+        driver.get("http://the-internet.herokuapp.com/nested_frames");
+
+        // step into the frame in which the web element is located
+        driver.switchTo().frame("frame-top").switchTo().frame("frame-left");
+
+        WebElement leftFrameBodyText = driver.findElement(By.xpath("//body"));
+
+        Assert.assertEquals(leftFrameBodyText.getText(), "LEFT");
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame("frame-middle");
+        WebElement middleFrameBodyText = driver.findElement(By.xpath("//body"));
+
+        Assert.assertEquals(middleFrameBodyText.getText(), "MIDDLE");
+    }
+
 
     @Test
     public void disappearingElements() {
@@ -152,9 +291,9 @@ public class HerokuAppTests {
         driver.get("https://admin:admin@the-internet.herokuapp.com/windows");
         WebElement clickHereLink = driver.findElement(By.cssSelector("a[href='/windows/new']"));
         clickHereLink.click();
-        String parentWindow= driver.getWindowHandle();
+        String parentWindow = driver.getWindowHandle();
         Set<String> allWindows = driver.getWindowHandles();
-        for(String curWindow : allWindows){
+        for (String curWindow : allWindows) {
             driver.switchTo().window(curWindow);
         }
         driver.switchTo().window(parentWindow);
